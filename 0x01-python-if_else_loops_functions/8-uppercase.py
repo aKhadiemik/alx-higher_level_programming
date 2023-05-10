c071a812c0ba0b7369691ea8f163ada7349bb7d4
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-09 19:51:26 -0700

    fix: no use of upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(string):
     chars = list(string)
     for i in range(len(chars)):
         if 'a' <= chars[i] <= 'z':
-            chars[i] = '{}'.format(chars[i].upper()) 
+            chars[i] = '{}'.format(chr(ord(chars[i]) - 32))
     print(''.join(chars))
