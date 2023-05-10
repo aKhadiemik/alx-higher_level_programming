ded7f0f2660f2bfc5fa7e24313a89c3dd7713fbf
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-09 19:15:57 -0700

    fix: soln fixed to output sequence only once

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-numbers = ["{:02d}".format(n) for n in range(90)]
-numbers[-1] += "\n"
-print(", ".join(numbers))
+for number in range(0, 89):
+    number, end = ((number + 1, '\n') if number == 88 else ((number + 1, ', ')))
+    number = (number + 9) if number % 10 == 0 else number
+    print("{:02d}".format(number), end=end)
