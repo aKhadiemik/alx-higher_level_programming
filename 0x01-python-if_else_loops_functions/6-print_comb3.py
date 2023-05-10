1361dad9998664510f447c51f802484d3f9f9b47
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-09 19:20:04 -0700

    fix: soln edited to output according to requirements

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for number in range(0, 89):
-    number, end = ((number + 1, '\n') if number == 88 else ((number + 1, ', ')))
-    number = (number + 9) if number % 10 == 0 else number
-    print("{:02d}".format(number), end=end)
+numbers = [(dig, git) for dig in range(0, 9) for git in range(dig+1, 10)]
+output = ", ".join(["{}{}".format(dig, git) for dig, git in numbers])
+print(output)
