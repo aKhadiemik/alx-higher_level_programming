449f123229ff211207a03eaf8ec4f1f6e2d620a1
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-11 08:40:34 -0700

    add: prints the number of and the list of its arguments

--- 0x02-python-import_module/1-calculation.py
+++ 0x02-python-import_module/1-calculation.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-import calculator_1 as calc
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    
+    import calculator_1 as calc
 
-add_result = calc.add(a, b)
-sub_result = calc.sub(a, b)
-mul_result = calc.mul(a, b)
-div_result = calc.div(a, b)
+    a = 10
+    b = 5
 
-print("The result of adding {} and {} is {}".format(a, b, add_result))
-print("The result of subtracting {} from {} is {}".format(b, a, sub_result))
-print("The result of multiplying {} and {} is {}".format(a, b, mul_result))
-print("The result of dividing {} by {} is {}".format(a, b, div_result))
+    add_result = calc.add(a, b)
+    sub_result = calc.sub(a, b)
+    mul_result = calc.mul(a, b)
+    div_result = calc.div(a, b)
+
+    print("The result of adding {} and {} is {}".format(a, b, add_result))
+    print("The result of subtracting {} from {} is {}".format(b, a, sub_result))
+    print("The result of multiplying {} and {} is {}".format(a, b, mul_result))
+    print("The result of dividing {} by {} is {}".format(a, b, div_result))
