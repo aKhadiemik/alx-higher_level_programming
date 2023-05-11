cb73b7f6f242ff21cd3e75a699e94136323a5e61
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-10 19:06:37 -0700

    add: fn checks for cycle in linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if linked list has a cycle
+ * @list: list to check
+ *
+ * Return: 1 if cycle exists, 0 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *t, *h;
+	if (!list)
+		return (0);
+	h = t = list;
+	while (h->next && h->next->next)
+	{
+		h = h->next->next;
+		t = t->next;
+		if (h == t)
+			return (1);
+	}
+	return (0);
+}
