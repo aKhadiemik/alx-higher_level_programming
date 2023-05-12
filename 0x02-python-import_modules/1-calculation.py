5ba9f12daf2333adb951d8466e385d21ee86b8f9
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-11 18:57:44 -0700

    fix: removed trailing spaces

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     """
         Outputs operands and results of operators
     """
-    from calculator_1 import add, sub, mul, div 
-    
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
