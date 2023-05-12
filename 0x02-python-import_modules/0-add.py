ad2d62ee74a3d779af31735f05d86a5003f93042
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-11 17:49:25 -0700

    add: imports the function def add(a, b), outputs result of operation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,13 +2,9 @@
 import add_0 as addtn
 
 
-def main():
+if __name__ == "__main__":
     a = 1
     b = 2
 
     result = addtn.add(a, b)
     print("{} + {} = {}".format(a, b, result))
-
-
-if __name__ == "__main__":
-    main() 
