ab5cebb89e025f646dcdb512afdddb6ce3e96b1e
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-14 17:06:19 -0700

    add: fn retrieves element from list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
+'''
+    element_at retrieves an element from a list
+'''
+
+
 def element_at(my_list, idx):
-	if idx < 0 or idx >= len(my_list):
-		return None
-	else:
-		return my_list[idx]
+    if idx < 0 or idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
