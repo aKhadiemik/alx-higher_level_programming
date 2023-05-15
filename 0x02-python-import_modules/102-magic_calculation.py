fa96288c86b5e5183dfc8411ec0ac5350a54185a
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-14 19:45:41 -0700

    add: Implements functions imported from magic_calculation_102

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    '''Implement operations in magic_calculation_102'''
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 7):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
