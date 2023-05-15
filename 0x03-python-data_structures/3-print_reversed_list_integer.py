2e8bc0a65a49aa75de34fe3c76c42b4026b975e7
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-14 17:36:31 -0700

    refactor: edited solution to use reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-'''
-    print_reversed_list_integer pritns list of integers in reverse
-'''
+"""
+    fn prints list of integers in reverse
+"""
 
 
 def print_reversed_list_integer(my_list):
