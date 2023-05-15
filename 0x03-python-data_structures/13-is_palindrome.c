7ec1d6379bd557d616d3fa996582964a3aafa7d9
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-14 18:57:08 -0700

    add: checks if singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,77 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if singly linked list is palindrome
- * @head: pointer to head of list
+ * is_palindrome - checks if singly linked list is palindrome.
+ *
+ * @head: double pointer to head of list
  *
  * Return: 0 if not palindrome, 1 if palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int values[1024], i, j;
+	listint_t *slow_ptr, *fast_ptr, *prev_slow_ptr = NULL;
+	listint_t *second_half, *mid_node = NULL;
+	int is_palindrome = 1;
 
-	if (*head == NULL)
-		return (1);
+	return ((*head == NULL || (*head)->next == NULL) ? 1 : 0);
 
-	for (i = 0; current != NULL; i++)
+	slow_ptr = *head, fast_ptr = *head;
+
+	while (fast_ptr != NULL && fast_ptr->next != NULL)
 	{
-		values[i] = current->n;
-		current = current->next;
+		fast_ptr = fast_ptr->next->next;
+		prev_slow_ptr = slow_ptr, slow_ptr = slow_ptr->next;
 	}
 
-	for (j = 0; j < i / 2; j++)
+	fast_ptr != NULL ? (mid_node = slow_ptr, slow_ptr = slow_ptr->next) : NULL;
+
+	second_half = slow_ptr,	prev_slow_ptr->next = NULL;
+	reverse_listint(&second_half);
+
+	while (*head != NULL && second_half != NULL)
 	{
-		if (values[j] != values[i - j - 1])
-			return (0);
+		if ((*head)->n != second_half->n)
+		{
+			is_palindrome = 0;
+			break;
+		}
+		*head = (*head)->next;
+		second_half = second_half->next;
 	}
 
-	return (1);
+	reverse_listint(&second_half);
+	if (mid_node != NULL)
+	{
+		prev_slow_ptr->next = mid_node;
+		mid_node->next = second_half;
+	}
+	else
+		prev_slow_ptr->next = second_half;
+
+	return (is_palindrome);
 }
 
+/**
+ * reverse_listint - reverses listint_t linked list
+ *
+ * @head: double pointer to head of list
+ *
+ * Return: pointer to first node of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
