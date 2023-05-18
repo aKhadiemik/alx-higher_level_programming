866091b93b9be093080d4493e8b960c6cdca1d7f
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-17 18:55:25 -0700

    add: fn returns a list with all values multiplied by a number without using any loops.

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3a
 def multiply_list_map(my_list=[], number=0):
-    '''
-        Returns new list multiplied by a number using map
-    '''
     return list(map(lambda x: x * number, my_list))
