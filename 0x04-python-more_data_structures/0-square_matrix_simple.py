9a6acf0c1e62bab133106b0403f53d825816afa8
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-17 18:01:41 -0700

    add: fn computes the square value of all integers of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    '''
+        Get the dimensions of the matrix
+    '''
+    rows = len(matrix)
+    cols = len(matrix[0])
+    
+    result = [[0] * cols for _ in range(rows)]
+    
+    for i in range(rows):
+        for j in range(cols):
+            result[i][j] = matrix[i][j] ** 2
+    
+    return result
