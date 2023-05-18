182498ac6254d9251cbf4610275fac85c34b957f
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-17 18:39:41 -0700

    add: replaces or adds key/value in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,7 +3,6 @@ def update_dictionary(a_dictionary, key, value):
     '''
         Update the value if the key already exists in the dictionary
     '''
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
+
+    return a_dictionary
