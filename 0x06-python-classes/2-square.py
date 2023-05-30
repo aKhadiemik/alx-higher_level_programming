fd8e541dc1f461b71a41c6b84ec6db48dd210304
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-30 11:50:17 -0700

    add: soln consists Square class with instantiated private instance var

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+    Module defines a Square class
+'''
+
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+        size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new instance of the Square class.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
