eca58264d6310f23ad8451f3dd66bf7cff117829
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-30 07:11:34 -0700

    add: soln consists an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This class represents a square.
+
+    Description:
+        The `Square` class is used to define a square object.
+        It does not have any attributes or methods.
+
+    Example:
+        >>> my_square = Square()
+        >>> print(type(my_square))
+        <class '__main__.Square'>
+        >>> print(my_square.__dict__)
+        {}
+    """
+    pass
