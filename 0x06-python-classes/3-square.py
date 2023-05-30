9c4a95f17ebe34d822a65b2fc5acb8b381d2d36d
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-30 11:56:49 -0700

    add: soln consists class with instantiated privinstance attr + public inst method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''
+    Module defines a square class
+'''
+
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+        size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new instance of the Square class.
+        area(self): Calculates and returns the area of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
