23de4151d297c51311c43f67ddb4188cfbcc3364
aKhadiemik <carolvsreyesz@gmail.com>
2023-05-30 12:07:21 -0700

    add: soln consists Square class + public instance method that prints out the square to stdout

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+'''
+    Module implements the Square class
+'''
+
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new instance of the Square class.
+        area(self): Calculates and returns the area of the square.
+        size(self): Getter method to retrieve the size of the square.
+        size(self, value): Setter method to set the size of the square.
+        my_print(self): Prints the square using the '#' character.
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """
+        Prints the square using the '#' character.
+
+        If size is equal to 0, an empty line is printed.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
