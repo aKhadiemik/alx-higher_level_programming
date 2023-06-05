8d3ff444724814c8f32d7d29e9bab57ecb165c23
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-05 08:24:35 -0700

    add: Rectangle class includes __del__() method and number_of_instances public instance attribute

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,13 +3,13 @@
     Module defines a rectangle class
 '''
 
+
 class Rectangle:
     """
     Represents a rectangle includes its width and height.
     """
 
-    number_of_instances = 0 
-
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """
