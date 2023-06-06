0eb12830fda356424f81e2fb78ac031aec066e88
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-06 04:51:30 -0700

    add: fn returns a string “BestSchool” n times the number of the iteration

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     if not hasattr(magic_string, "count"): magic_string.count = 0
-    return 'BestSchool$' if not hasattr(magic_string, "count") \
-        else ', '.join(['BestSchool'] * (magic_string.count + 1))
+    return 'BestSchool' if not hasattr(magic_string, "count") else ', '.join(['BestSchool'] * (magic_string.count + 1))
