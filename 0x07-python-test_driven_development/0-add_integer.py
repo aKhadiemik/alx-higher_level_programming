d8a340d9cdcbd18699fffdf2a13fc32367e9a0af
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-06 13:52:34 -0700

    add: module with fn to sum 2 ints

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+    Module contains function to sum 2 ints
+'''
+
+
+def add_integer(a, b=98):
+  """
+  Adds 2 integers.
+
+  Args:
+    a: The first integer to add.
+    b: The second integer to add. Defaults to 98.
+
+  Returns:
+    The sum of a and b.
+
+  Raises:
+    TypeError: If a or b is not an integer.
+  """
+
+  if not isinstance(a, (int, float)):
+    raise TypeError("a must be an integer")
+
+  if not isinstance(b, (int, float)):
+    raise TypeError("b must be an integer")
+
+  a = int(a)
+  b = int(b)
+
+  return a + b
