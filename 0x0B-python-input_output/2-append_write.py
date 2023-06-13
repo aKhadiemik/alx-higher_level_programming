89456ae1fb966389634e954efd1117efb25ab034
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-13 01:22:27 -0700

    Provides fn to append string to end of text file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Provides fn to append string to end of text file'''
+
+
+def append_write(filename="", text=""):
+    """
+        Append string to end of text file, return number of characters added.
+
+        Args:
+            filename (str): name of file to which the string should be appended.
+            text (str): The string to be appended to the file.
+
+        Returns:
+            int: The number of characters added to the file.
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        num_characters_added = file.write(text)
+    return num_characters_added
