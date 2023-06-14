59a33a9c42b2d33a9d53718f5ce41548815e3906
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-14 06:12:01 -0700

    fix: documentation addede for function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,5 +5,15 @@
 
 
 def lookup(obj):
+    """
+    Retrieve a list of attributes from an object that are not callable or
+    start with '__'.
+
+    Args:
+    obj: An object to perform the attribute lookup on.
+
+    Returns:
+    A list of attributes that are not callable or start with '__'.
+    """
     return [attr for attr in dir(obj) if not callable(getattr(obj, attr))
             or attr.startswith("__")]
