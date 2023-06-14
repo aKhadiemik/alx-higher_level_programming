63b340ec57adffa521152d422063dcc588d60380
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-14 06:36:51 -0700

    fix: documentation added for empty class - BaseGeometry

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Provides empty BaseGeometry class"""
+
 
 class BaseGeometry:
+    """
+    A base class representing a generic geometry.
+
+    This class does not contain any methods or attributes.
+    """
     pass
