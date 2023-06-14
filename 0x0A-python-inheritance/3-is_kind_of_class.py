a766eb1ed32f5a2986ee2340057d7618a22bbc64
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-14 06:45:31 -0700

    fix: edits to function documentation

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,14 @@
 #!//bin/python3
 """
     Provides fn that checks whether object is instance of specified
-    class/subclass
+        class/subclass
 """
 
 
 def is_kind_of_class(obj, a_class):
     """
-    Check if an object is an instance of a specified class or its subclass.
+    Check if an object is an instance of a specified class or
+        its subclass.
 
     Args:
         obj: The object to be checked.
@@ -15,6 +16,6 @@ def is_kind_of_class(obj, a_class):
 
     Returns:
         True if the object is an instance of the specified class or its
-                                                subclass, False otherwise.
+            subclass, False otherwise.
     """
     return isinstance(obj, a_class)
