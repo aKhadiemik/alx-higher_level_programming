8e368d08fd8dc9bd30b47a16ad307aa8389532e6
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-14 06:34:54 -0700

    fix: documentation added for inheritance checker

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""Provides fn that checks for inheritance"""
+
 
 def inherits_from(obj, a_class):
+    """
+    Check if an object inherits from a specific class.
+
+    Args:
+    obj: The object to be checked.
+    a_class: The class to check against.
+
+    Returns:
+    A boolean value indicating whether the object inherits from the class.
+    """
     return isinstance(obj, a_class) and type(obj) is not a_class
