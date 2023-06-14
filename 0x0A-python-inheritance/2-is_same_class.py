001595e4d53d39df15a9ba239e0b309b6bd0a232
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-14 06:29:43 -0700

    fix: documentation added

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""Provides fn that checks whether object belogns to specific class"""
+
 
 def is_same_class(obj, a_class):
+   """
+   Check if an object belongs to a specific class.
+
+   Args:
+   obj: An object to be checked.
+   a_class: A class to compare the object's type with.
+
+   Returns:
+   True if the object belongs to the specified class, False otherwise.
+  """
     return type(obj) is a_class
