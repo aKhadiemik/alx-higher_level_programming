c2e43c393f5a70f46a08602329444d16f7d8fa47
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-14 15:57:59 -0700

    fix: added documentation for BaseGeometry class and its area method

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""Provides class with area method"""
+
 
 class BaseGeometry:
+    """
+    A base class for geometry calculations.
+
+    Methods:
+    area: Raises an exception indicating that it is not implemented.
+    """
     def area(self):
+        """
+        Calculate the area of the geometry.
+
+        Raises:
+        Exception: Indicates that the method is not implemented.
+        """
         raise Exception("area() is not implemented")
