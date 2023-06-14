07aca21296ca96402f367461d70860ee46a74094
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-14 06:18:08 -0700

    fix: documentation added to conform to pycodestyle requirements

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""Provides class that extends built-in list class"""
+
 
 class MyList(list):
+    """
+    A custom list class that extends the built-in list class.
+
+    This class provides an additional method, print_sorted(), which
+    sorts the list and prints the sorted elements.
+    """
+
     def print_sorted(self):
+        """
+        Sort list in ascending order and print the sorted elements.
+        """
         sorted_list = sorted(self)
         print(sorted_list)
