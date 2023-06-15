d737c35ed261015df52bfb0aba0c8b1b82075c99
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-14 17:13:25 -0700

    fix: doc added for add_attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
+'''Module provides add_attribute method'''
+
 
 def add_attribute(obj, attribute, value):
+    """
+    Adds a new attribute to an object.
+
+    Args:
+    obj: The object to which the attribute will be added.
+    attribute: The name of the attribute.
+    value: The value to be assigned to the attribute.
+
+    Raises:
+    TypeError: If the object does not support dynamic attributes.
+
+    Example:
+        >>> obj = SomeClass()
+        >>> add_attribute(obj, 'new_attribute', 42)
+        >>> print(obj.new_attribute)
+    """
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, attribute, value)
