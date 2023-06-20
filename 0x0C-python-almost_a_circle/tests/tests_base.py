4062706ecf68dacc2be58f41958d496011e3ab26
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-19 19:56:27 -0700

    feat: added test classes for the base module

--- 0x0C-python-almost_a_circle/tests/tests_base.py
+++ 0x0C-python-almost_a_circle/tests/tests_base.py
@@ -5,16 +5,41 @@ from models.square import Square
 import json
 import inspect
 """
+Base module test cases.
 """
 
 
 class tests_base(unittest.TestCase):
-    """"""
+    """
+    Defines test cases.
+    """
     def test_id(self):
-        """"""
+        """
+        Valid id.
+        """
         base = Base(99)
         self.assertEqual(99, base.id)
 
+    def test_no_id(self):
+        """
+        No id.
+        """
+        base = Base()
+        self.assertEqual(99, base.id)
+
+    def test_neg_id(self):
+        """
+        Negative id.
+        """
+        base = Base(-99)
+        self.assertEqual(-99, base.id)
+
+    def test_zero_id(self):
+        """
+        Zero as id.
+        """
+        base = Base(0)
+        self.assertEqual(0, base.id)
 
 class tests_square(unittest.TestCase):
     """"""
