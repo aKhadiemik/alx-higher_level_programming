ebf0dcb23273b827139d1a2e18060ba877d3d282
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-19 18:54:15 -0700

    add: initial commit for tests

---
+++ 0x0C-python-almost_a_circle/tests/tests_base.py
@@ -0,0 +1,22 @@
+#/usr/bin/python3
+import unittest
+from models.base import Base
+from models.square import Square
+import json
+import inspect
+"""
+"""
+
+
+class tests_base(unittest.TestCase):
+    """"""
+    def test_id(self):
+        """"""
+        base = Base(99)
+        self.assertEqual(99, base.id)
+
+class tests_square(unittest.TestCase):
+    """"""
+    def test_docstring_exists(self):
+        """"""
+        self.assertTrue(len(Base.__doc__) >= 1)
