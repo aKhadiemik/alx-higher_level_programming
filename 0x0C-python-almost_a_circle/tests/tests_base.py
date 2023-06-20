ee54a79a8402e055e987c3f38f346f91b684ce41
aKhadiemik <carolvsreyesz@gmail.com>
2023-06-20 03:16:12 -0700

    fix: edited sequence of base tests

--- 0x0C-python-almost_a_circle/tests/tests_base.py
+++ 0x0C-python-almost_a_circle/tests/tests_base.py
@@ -9,22 +9,23 @@ Base module test cases.
 """
 
 
-class tests_base(unittest.TestCase):
+class TestsBase_cases(unittest.TestCase):
     """
     Defines test cases.
     """
-    def test_id(self):
-        """
-        Valid id.
-        """
-        base = Base(99)
-        self.assertEqual(99, base.id)
 
     def test_no_id(self):
         """
         No id.
         """
         base = Base()
+        self.assertEqual(99, base.id)
+
+    def test_id(self):
+        """
+        Valid id.
+        """
+        base = Base(99)
         self.assertEqual(99, base.id)
 
     def test_neg_id(self):
@@ -40,9 +41,3 @@ class tests_base(unittest.TestCase):
         """
         base = Base(0)
         self.assertEqual(0, base.id)
-
-class tests_square(unittest.TestCase):
-    """"""
-    def test_docstring_exists(self):
-        """"""
-        self.assertTrue(len(Base.__doc__) >= 1)
