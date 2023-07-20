3d789f3ca4e4341e721566c891f16092d7abd4d0
aKhadiemik <carolvsreyesz@gmail.com>
2023-07-20 04:22:16 -0700

    add: creates db and table as specified

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- Creates hbtn_0d_usa db and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Picks db for querying
+USE hbtn_0d_usa;
+-- Creates table
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id), FOREIGN KEY(state_id) REFERENCES states(id));
